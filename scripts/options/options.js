93d343c11456bae456a7f15a4a8c2ac2ae28d989
Tomas C <aresmta@gmail.com>
2016-05-22 19:23:55 -0300

    Update options.js

--- scripts/options/options.js
+++ scripts/options/options.js
@@ -1,7 +1,20 @@
 function save_options() {
   var updateDelay = document.getElementById('updateDelay').value;
   var enableNotifications = document.getElementById('enableNotifications').checked;
-  if ( typeof updateDelay != "number" ) { updateDelay = 15; document.getElementById('updateDelay').value = 15;}
+  console.log(typeof updateDelay);
+    if ( typeof updateDelay != "number" ) {
+	  	if ( typeof Number(updateDelay) != "number" ) {
+	   		updateDelay = 15; document.getElementById('updateDelay').value = 15;
+	  	} else {
+	  		if ( Number(updateDelay) > 60 ) {
+	  			updateDelay = 60; document.getElementById('updateDelay').value = 60;
+	  		} else {	
+	  			if ( Number(updateDelay) < 10 ) {
+	  				updateDelay = 10; document.getElementById('updateDelay').value = 10;
+	  			}
+	  		}
+	  	}
+	}
   chrome.storage.sync.set({
     updateDelay: updateDelay,
     enableNotifications: enableNotifications
