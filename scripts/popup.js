09ef0cbfb3ea2203bb7b97ff412cbb44d5ab48f0
Tomas C <aresmta@gmail.com>
2017-03-18 19:31:52 -0300

    Update popup.js

--- scripts/popup.js
+++ scripts/popup.js
@@ -14,55 +14,57 @@ if ( Object.keys(notifications).length > 0) {
 	for(key in notifications) {
 		var item = notifications[key];
 		
-		var board = item[0];
-		var discussion = item[1];
-		var comment = item[2];
-		var parentDiscussion = item[3];
-		var parentDiscussion_url = item[4];
-		var profile_url = item[5];
-		var profile_sn = item[6];
-		var server = item[7];
-		var timeago = item[8];
-		var url = item[9];
-		var message = item[10];
-		var isRioter = item[11];
-		
-		var avatar = isRioter && `/images/riot_fist.png` || `https://avatar.leagueoflegends.com/las/${profile_sn}.png`;
-		var fontcolor = isRioter && `<font color=#ae250f> ${profile_sn} </font>` || profile_sn;
-		source = source + 
-		`<div class='update-item' data-application-id=${board} data-discussion-id=${discussion} data-comment-id=${comment}>
-			<div class='parent-comment'> 
-				<span class='in'>en</span> 
-				<a href=${parentDiscussion_url} target='_blank'>${parentDiscussion}</a> 
-			</div>
-			<div class='comment clearfix'> 
-				<div class='header byline clearfix'> 
-					<div class='inline-profile'>
-						<a href=${profile_url} target='_blank'>
-							<span class='icon'>
-								<img src=${avatar}>
-							</span>
-							<span class='username'>
-								${fontcolor}
-							</span>
-						</a>
-						<span class='realm'>
-							${server}
-						</span>
-					</div>
-					<span class='timeago' title=${timeago}> </span> 
+		if (item) {
+			var board = item[0];
+			var discussion = item[1];
+			var comment = item[2];
+			var parentDiscussion = item[3];
+			var parentDiscussion_url = item[4];
+			var profile_url = item[5];
+			var profile_sn = item[6];
+			var server = item[7];
+			var timeago = item[8];
+			var url = item[9];
+			var message = item[10];
+			var isRioter = item[11];
+			
+			var avatar = isRioter && `/images/riot_fist.png` || `https://avatar.leagueoflegends.com/las/${encodeURIComponent(profile_sn)}.png`;
+			var fontcolor = isRioter && `<font color=#ae250f> ${profile_sn} </font>` || profile_sn;
+			source = source + 
+			`<div class='update-item' data-application-id=${board} data-discussion-id=${discussion} data-comment-id=${comment}>
+				<div class='parent-comment'> 
+					<span class='in'>en</span> 
+					<a href=${parentDiscussion_url} target='_blank'>${parentDiscussion}</a> 
 				</div>
-				<div class='body markdown-content'>
-					${message}
-				</div> 
-				<div class='footer'>
-					<div class='right'>
-						<a href=${url} target='_blank'>Ir a comentario</a>
+				<div class='comment clearfix'> 
+					<div class='header byline clearfix'> 
+						<div class='inline-profile'>
+							<a href=${profile_url} target='_blank'>
+								<span class='icon'>
+									<img src=${avatar}>
+								</span>
+								<span class='username'>
+									${fontcolor}
+								</span>
+							</a>
+							<span class='realm'>
+								${server}
+							</span>
+						</div>
+						<span class='timeago' title=${timeago}> </span> 
+					</div>
+					<div class='body markdown-content'>
+						${message}
+					</div> 
+					<div class='footer'>
+						<div class='right'>
+							<a href=${url} target='_blank'>Ir a comentario</a>
+						</div> 
 					</div> 
 				</div> 
-			</div> 
-		</div>`
-		document.getElementById("my-updates").innerHTML = source;
+			</div>`
+			document.getElementById("my-updates").innerHTML = source;
+		}
 	}
 }
 
@@ -90,6 +92,7 @@ document.getElementById("clear").onclick = function() {
 		comments[`comments[${board}][${discussion}][]`+" ".repeat(i+1)] = comment;
 	}	
 	chrome.extension.getBackgroundPage().setAsRead(comments);
+	window.close();
 };
 
 document.getElementById("about").onclick = function() {
